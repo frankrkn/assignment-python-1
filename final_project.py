b6457229af08efaeb33e7018bd8375dee92290b2
frankrkn <furkanerkin@icloud.com>
2026-01-21 14:01:26 +0100

    eighth commit

--- final_project.py
+++ final_project.py
@@ -62,3 +62,42 @@ for word in words:
         
         else:
             word_freq[word] = 1
+            
+##I will use three example texts pulled directly online, topic detection will be limited to those texts. I'd have liked to go for a more accurate value driven keyword strategies
+##but that sounds like a semester long project to hone it instead of a few weeks long.
+
+topics = {
+    "data_science_python": [
+        "python", "data", "science", "machine", "learning", "ai",
+        "analytics", "libraries", "pandas", "numpy", "model",
+        "dataset", "algorithm", "statistics", "automation", "career"
+    ],
+
+    "wildlife_conservation": [
+        "snow", "leopard", "wildlife", "conservation", "species",
+        "population", "habitat", "ecosystem", "endangered",
+        "protection", "biodiversity", "monitoring", "research",
+        "mountain", "community"
+    ],
+
+    "digestive_health": [
+        "digestive", "digestion", "stomach", "intestine",
+        "nutrients", "absorption", "gut", "bacteria",
+        "fibre", "diet", "nutrition", "hydration",
+        "water", "exercise", "health"
+    ]
+}
+
+topic_scores = {}
+
+for topic in topics:
+  topic_scores[topic] = 0
+
+for word in word_freq:
+  for topic in topic_scores:
+    if word in topics[topic]:
+      topic_scores[topic] = topic_scores[topic] + word_freq[word]
+
+detected_topic = max(topic_scores, key=topic_scores.get)
+
+print("Detected topic is:", detected_topic)
